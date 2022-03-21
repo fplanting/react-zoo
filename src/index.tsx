43a6f3815d20c549c1de743ff22ba539d1ae323b
fplanting <frida.planting@hotmail.com>
2022-03-21 09:45:26 +0100

    added routes

---
+++ src/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home';
+import { Details } from './components/Details';
+import { NotFound } from './components/NotFound';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/Home" element={<Home />} />
+      <Route path="/Details" element={<Details />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    </BrowserRouter>
+
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
