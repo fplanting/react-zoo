7001f240875df99882c592d00f77849f73da5150
fplanting <frida.planting@hotmail.com>
2022-03-24 13:46:21 +0100

    added css and fonts

--- src/components/DisplayAnimal.tsx
+++ src/components/DisplayAnimal.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Animal } from "../models/Animal";
 import IAnimal from "../models/IAnimal";
 import AnimalService from "../services/AnimalService";
@@ -33,20 +33,22 @@ export const DisplayAnimal = () => {
             <h1 className="name">{animal?.name}</h1>
                 <div className="animalWrapper">
                     <img className="image" src={animal?.imageUrl}></img>
-                    <h2 className="born">Född:</h2>
-                    <p className="animalBorn">{animal?.yearOfBirth}</p>
-                    <h2 className="description">Beskriving:</h2>
+                    <div className="wrapperInfo">
+                    <h2 className="description">Om {animal?.name}</h2>
+                    <p className="animalBorn"><b>Född:</b> {animal?.yearOfBirth}</p>
+                    <p className="animalMedicine"><b>Medicin:</b> {animal?.medicine}</p>
                     <p className="animalDescriptionLong">{animal?.longDescription}</p>
-                    <h2 className="medicine">Medicin:</h2>
-                    <p className="animalMedicine">{animal?.medicine}</p>
-                    <h2 className="fed">Matad/ej matad:</h2>
-                    <p className="animalFed">{animal?.isFed ? "Matad" : "Ej matad"}</p>
-                    <h2 className="lastFed">Senast matad:</h2>
+                    </div>
+                    <div className="wrapperFed">
+                    <h2 className="fed">Matad/inte matad</h2>
+                    <p className="animalFed">{animal?.isFed ? "Matad" : "inte matad"}</p>
+                    <h2 className="lastFed">Senast matad</h2>
                     <p className="animalLastFed">{animal?.lastFed}</p>
                     <button onClick={() => {
                          AnimalService.setLS(animal);
                         }} disabled={animal?.isFed}>Mata djuret</button>
-
+                 </div>
+                 <Link className="link" to="/">Gå tillbaka och se fler djur</Link>
             </div>
         </div>
     );
